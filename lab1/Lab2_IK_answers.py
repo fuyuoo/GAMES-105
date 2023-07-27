3ea7413ca17be591ebc41e1368e86cda4632b33a
fuyuoo <574246399@qq.com>
2023-07-27 18:53:48 +0800

    1

--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -4,24 +4,32 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
-def FK(start, path, joint_offsets_path, joint_rotations_path, joint_positions, joint_orientations):
-    for index in range(start - 1, len(path) - 1):
-        joint_index = index + 1
-        parent_index = index
-        if joint_index == 0:
-            Q1 = joint_rotations_path[joint_index]  # 全局朝向
-            P1 = joint_positions[path[joint_index]]
+def FK(path, joint_names,joint_parents, joint_offsets, joint_rotation):
+
+    joint_positions = []
+    joint_orientations = []
+    for i in range(len(joint_names)):
+        if i in path:
+            continue
+        parent_index = joint_parents[i]
+        if parent_index == -1:
+            Q1 = R.from_euler('XYZ', joint_rotation[0], degrees=True)  # 全局朝向
+            P1 = joint_position_temp
         else:
-            Q0 = R.from_quat(joint_orientations[path[parent_index]])
-            R1 = joint_rotations_path[joint_index]
-            P0 = joint_positions[path[parent_index]]
-            L0 = joint_offsets_path[joint_index]
+            Q0 = R.from_quat(joint_orientations[parent_index])
+            R1 = joint_rotation[i]
+            P0 = joint_positions[parent_index]
+            L0 = joint_offsets[i]
 
             # 计算当前关节的全局位置和朝向
-            Q1 = Q0 * R1
+            Q1 = Q0 * R.from_euler('XYZ', R1, degrees=True)
             P1 = P0 + Q0.apply(L0)
-        joint_positions[path[joint_index]] = P1
-        joint_orientations[path[joint_index]] = Q1.as_quat()
+
+        joint_positions.append(P1)
+        joint_orientations.append(Q1.as_quat())
+
+    return np.array(joint_positions), np.array(joint_orientations)
+
 
 
 def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, target_pose):
@@ -42,22 +50,32 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
     path, _, _, _ = meta_data.get_path_from_root_to_end()
 
     # init rotation
-    joint_rotations_path = [R.from_quat(joint_orientations[0])]
+    joint_names = meta_data.joint_name
+    joint_parents = meta_data.joint_parent
+
+    joint_rotations = [R.from_quat(joint_orientations[0])]
     for i in range(1, len(path) - 1):
         orientation_A = joint_orientations[path[i]]
         orientation_B = joint_orientations[path[i + 1]]
         rotation_offset = np.multiply(orientation_A, np.conj(orientation_B))
-        joint_rotations_path.append(R.from_quat(rotation_offset))
-    joint_rotations_path.append(R.from_quat([0, 0, 0, 1]))
+        joint_rotations.append(R.from_quat(rotation_offset))
+    joint_rotations.append(R.from_quat([0, 0, 0, 1]))
+
+    for i in range(len(joint_names)):
+        if joint_names[i].endswith("_end"):
+            joint_rotations.append([0, 0, 0])
+        else:
+            joint_rotation.append([motion_data[num], motion_data[num + 1], motion_data[num + 2]])
 
     # init offset
     joint_initial_position = meta_data.joint_initial_position
-    joint_offsets_path = [np.array([0, 0, 0])]
-    for i in range(1, len(path)):
-        position_A = joint_initial_position[path[i]]
-        position_B = joint_initial_position[path[i - 1]]
+    joint_offsets = [np.array([0, 0, 0])]
+    for i in range(1, len(joint_names)):
+        parent_index = joint_parents[i]
+        position_A = joint_initial_position[parent_index]
+        position_B = joint_initial_position[i]
         offset = position_A - position_B
-        joint_offsets_path.append(offset)
+        joint_offsets.append(offset)
 
     min_distance = np.linalg.norm(target_pose - joint_positions[path[-1]])
     cnt = 0
@@ -66,13 +84,16 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
             cur_position = joint_positions[path[i]]
             target_vector = target_pose - cur_position
             end_vector = joint_positions[path[-1]] - cur_position
-            rotate_quat = R.align_vectors([end_vector], [target_vector])[0]
-            joint_rotations_path[i] = rotate_quat * joint_rotations_path[i]
-            FK(i, path, joint_offsets_path, joint_rotations_path, joint_positions, joint_orientations)
+            rotate_quat = R.align_vectors([target_vector],[end_vector])[0]
+            joint_rotations[path[i]] = rotate_quat * joint_rotations[path[i]]
+            joint_orientations[path[i]] = (rotate_quat * R.from_quat(joint_orientations[path[i]])).as_quat()
+            for j in range(i,len(path) - 1):
+                joint_positions[path[j+1]] = joint_positions[path[j]] + R.from_quat(joint_orientations[path[j]]).apply(joint_offsets[path[j + 1]])
             min_distance = min(min_distance, np.linalg.norm(target_pose - joint_positions[path[-1]]))
             if min_distance <= 0.01:
                 break
         cnt += 1
+    FK(path, joint_names,joint_parents, joint_positions, joint_orientations)
 
     return joint_positions, joint_orientations
 
